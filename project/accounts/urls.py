5e774f89d6a71f3420b4b2d3c5c34849a3477caf
mojtaba <mojtaba.moazen.a@gmail.com>
2021-02-05 12:41:39 +0330

    1- send data to the query page contain ID , ... completed 2- logout btn completed (need style)

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -3,9 +3,8 @@ from . import views
  
  
 urlpatterns = [
-    # path('', views.home, name='accounts' ),
+    #path('', views.home, name='accounts' ),
     path('login/', views.login, name = 'login'),
-     path('mainpage/', views.loggedin, name = 'loggedin'),
-    # path('logout/', views.logout, name = 'logout'),
-    # path('signup/', views.signup, name='signup')
+    path('logout/', views.logout, name = 'logout'),
+   # path('signup/', views.signup, name='signup')
     ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render , HttpResponse , redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
         user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
+            context = {}
+            context['id'] = User.objects.get(username=user.username).pk
+            context['full_name'] = User.objects.get(username=user.username).username
             auth.login(request, user)
             # . . . 
-            return redirect('../mainpage' , )
+            return render(request , '../Templates/Query.html' , context)
        
         else:
             return render(request, '../Templates/login.html', {'error':'Invalid Username Or Password'})
     else:
         return render(request , '../Templates/login.html')
 
-def loggedin(request):
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return render(request,'../Templates/home.html')
     
