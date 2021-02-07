f29b017e2f71304958ebd4a57a6c5593e631ed8a
fatemeh-ra <fatemeh.r3366@gmail.com>
2021-02-07 16:17:47 +0330

    log updated

--- project/hospital/Targets.py
+++ project/hospital/Targets.py
@@ -2,7 +2,7 @@ from django.db import connection
 
 
 def check_targets(query_from, query_where, target):
-    """Output: 0 if all targets are valid, 0 otherwise"""
+    """Output: 0 if all targets are valid, 1 otherwise"""
     cursor = connection.cursor()
     result_set = None
     success = 0
@@ -28,7 +28,8 @@ def check_targets(query_from, query_where, target):
     pass
 
 
-def log_access(query_from, query_where, target, subject_id):
+def log_access(query_from, query_where, target, subject_id, read_write):
+    """Input read_write: 0 for raed Query, 1 for write query"""
     cursor = connection.cursor()
     result_set = None
     try:
@@ -38,7 +39,26 @@ def log_access(query_from, query_where, target, subject_id):
     finally:
         cursor.close()
 
-    for i in result_set:
+    cursor = connection.cursor()
+    result_set2 = None
+    if read_write == 0:
+        try:
+            Query = "select * from read_access(%s)" % (subject_id,)
+            cursor.execute(Query)
+            result_set2 = cursor.fetchall()
+        finally:
+            cursor.close()
+    elif read_write == 1:
+        try:
+            Query = "select * from write_access(%s)" % (subject_id,)
+            cursor.execute(Query)
+            result_set2 = cursor.fetchall()
+        finally:
+            cursor.close()
+
+    # print(result_set, result_set2, list(set(result_set) & set(result_set2)))
+    
+    for i in list(set(result_set) & set(result_set2)):
         cursor = connection.cursor()
         try:
             cursor.execute("insert into Access_Log values(default, %s, %s, %s)", (subject_id, i, target, ))
